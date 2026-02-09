69939c1d9f0a87378421657520392680f731be1d
astroflann <79231568+astroflann@users.noreply.github.com>
2026-02-09 16:48:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,35 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+
+let onlineUsers = {};
+
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('A user connected', socket.id);
+
+
+    socket.emit('online users', Object.values(onlineUsers));
+
+    socket.on('mark online', (data) => {
+        onlineUsers[socket.id] = data.username;
+        console.log('server received mark online:', data.username);
+
+ 
+        io.emit('mark online', { username: data.username });
+    });
 
     socket.on('chat message', (data) => {
         io.emit('chat message', data);
     });
+
+    socket.on('disconnect', () => {
+        const username = onlineUsers[socket.id];
+        if (username) {
+            delete onlineUsers[socket.id];
+            io.emit('user disconnected', { username });
+            console.log('User disconnected:', username);
+        }
+    });
 });
 
-http.listen(3000);
+http.listen(3000, () => console.log('Server running on port 3000'));
